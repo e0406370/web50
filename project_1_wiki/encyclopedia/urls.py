d7aaf8b6ab1374540d81b7fc77444a19902c24f6
e0406370 <e0406370@u.nus.edu>
2024-03-04 11:02:47 +0800

    Completed implementation for e. Edit Page

--- project_1_wiki/encyclopedia/urls.py
+++ project_1_wiki/encyclopedia/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("error", views.entry, name="error"),
-    path("wiki/<str:entry_title>", views.entry, name="wiki"),
+    path("errorwiki", views.view_entry, name="errorwiki"),
+    path("wiki/<str:entry_title>", views.view_entry, name="wiki"),
+    path("erroredit", views.edit_entry, name="erroredit"),
+    path("edit/<str:entry_title>", views.edit_entry, name="edit"),
+    path("save/<str:entry_title>", views.save_entry, name="save"),
     path("random", views.random_entry, name="random")
 ]
--- project_1_wiki/encyclopedia/views.py
+++ project_1_wiki/encyclopedia/views.py
@@ -2,35 +2,79 @@ from . import util
 
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django import forms
 
 from markdown2 import Markdown
 import random
 
-def index(request):
-    
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
 
-def entry(request, entry_title):
-    
+def index(request):
+
+    return render(
+        request, 
+        "encyclopedia/index.html", 
+        {"entries": util.list_entries()}
+    )
+
+
+def view_entry(request, entry_title):
+
     entry_contents = util.get_entry(entry_title)
-    
-    if (entry_contents == None):
-        return render (request, "encyclopedia/error.html", {
-            "entry_title": entry_title
-        })
-    
+
+    if entry_contents == None:
+        return render(
+            request, 
+            "encyclopedia/error.html", 
+            {"entry_title": entry_title}
+        )
+
     markdowner = Markdown()
     entry_contents_converted = markdowner.convert(entry_contents)
-    
-    return render (request, "encyclopedia/entry.html", {
-        "entry_title": entry_title,
-        "entry_contents": entry_contents_converted
-    })
-    
+
+    return render(
+        request,
+        "encyclopedia/entry.html",
+        {"entry_title": entry_title, "entry_contents": entry_contents_converted},
+    )
+
+
+def edit_entry(request, entry_title):
+
+    entry_contents = util.get_entry(entry_title)
+
+    if entry_contents == None:
+        return render(
+            request, 
+            "encyclopedia/error.html", 
+            {"entry_title": entry_title}
+        )
+
+    markdowner = Markdown()
+    entry_contents_converted = markdowner.convert(entry_contents)
+
+    return render(
+        request,
+        "encyclopedia/edit.html",
+        {"entry_title": entry_title, "entry_contents": entry_contents_converted},
+    )
+
+
+def save_entry(request, entry_title):
+
+    if request.method == "POST":
+
+        entry_contents = request.POST.get("edit").strip()
+        
+        markdowner = Markdown()
+
+        util.save_entry(entry_title.capitalize(), markdowner.convert(entry_contents))
+        
+        return redirect("wiki", entry_title=entry_title)
+
+
+        
 def random_entry(request):
-    
+
     random_entry = random.choice(util.list_entries())
-    
-    return redirect("wiki", entry_title = random_entry)
+
+    return redirect("wiki", entry_title=random_entry)

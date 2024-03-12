7ce047f5b3f0adb41f9445d6f9cae523b0d51c8e
e0406370 <e0406370@u.nus.edu>
2024-03-13 07:35:58 +0800

    Project 2 => completed models

--- project_2_commerce/auctions/models.py
+++ project_2_commerce/auctions/models.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+# Comment -> User (1 Comment -> 1 User, 1 User -> Many Comments): OneToMany
+# Bid -> User (1 Bid -> 1 User, 1 User -> Many Bids): OneToMany
+# Listing -> User (1 Listing -> 1 User, 1 User -> Many Listings): OneToMany
+# applies to Comment -> Listing, Bid -> Listing 
 
 class User(AbstractUser):
     pass
 
-
+# foreign key to user
 class Listing(models.model):
 
     ## DEFINED IN 'Create Listing' FORM
@@ -18,6 +22,19 @@ class Listing(models.model):
     ## INITIALISED UPON CREATION
     listing_id = models.UUIDField()  # path variable for accessing listings
     creation_date = models.DateTimeField(auto_now_add=True)  # when the listing was created
-    creation_user = models.CharField() # user who created the listing
-    comments = models.JSONField(default=list, blank=True, null=True) # comment field
-    bid_quantity = models.IntegerField(default=0) # bid field
+    creation_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="lister")  # User who created the listing
+
+# foreign key to user
+# foreign key to listing
+class Bid(models.model):
+    bid_amount = models.FloatField()  # value of bid in decimal
+    bid_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder")  # User linked to this bid
+    bid_listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing")  # Listing linked to this bid
+
+
+# foreign key to user
+# foreign key to listing
+class Comment(models.model):
+    comment_text = models.CharField(max_length=256)  # value of comment in string
+    comment_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")  # User linked to this comment
+    comment_listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing")  # Listing linked to this comment

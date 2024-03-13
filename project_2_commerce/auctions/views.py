721ee75a95bc22aced8ba0584c260897d7ccd4ef
e0406370 <e0406370@u.nus.edu>
2024-03-13 10:33:30 +0800

    Project 2 => completed implementation of view all active listings page

--- project_2_commerce/auctions/views.py
+++ project_2_commerce/auctions/views.py
@@ -11,7 +11,14 @@ from .models import Listing, User
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+
+    listings = Listing.objects.all()
+
+    return render(
+        request, 
+        "auctions/index.html",
+        {"listings": listings}
+    )
 
 
 def login_view(request):

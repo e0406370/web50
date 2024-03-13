ceed0cb0839e23506900e88f8ebc3d05a5f4a6bc
e0406370 <e0406370@u.nus.edu>
2024-03-13 21:44:26 +0800

    Project 2 => updated admin.py with list_display of properties

--- project_2_commerce/auctions/admin.py
+++ project_2_commerce/auctions/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from auctions.models import Listing, Bid, Comment, WatchList
 
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description", "starting_bid", "image_url", "categories", "creation_date", "creation_user", "active_state")
+    
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("bid_amount", "bid_user", "bid_listing")
+    
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("comment_text", "comment_user", "comment_listing")
+       
+class WatchListAdmin(admin.ModelAdmin):
+    list_display = ("user", "listing")
+    
 # Register your models here.
-admin.site.register(Listing)
-admin.site.register(Bid)
-admin.site.register(Comment)
-admin.site.register(WatchList)
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Bid, BidAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(WatchList, WatchListAdmin)

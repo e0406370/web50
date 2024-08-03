93a5d416a43460a34d8174565a8bde7a0df434a2
e0406370 <e0406370@u.nus.edu>
2024-08-04 02:43:10 +0800

    Project 3 => cleared Task 1 : Send Mail

--- project_3_mail/mail/static/mail/inbox.js
+++ project_3_mail/mail/static/mail/inbox.js
@@ -2,9 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', compose_email);
+
+  // Task 1: Send email via compose form
+  document.querySelector('#compose-form').onsubmit = send_email;
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -18,7 +21,7 @@ function compose_email() {
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-subject').value = '';  
   document.querySelector('#compose-body').value = '';
 }
 
@@ -30,4 +33,29 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email() {
+
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+  console.info(recipients, subject, body);
+
+  fetch('/emails', {
+    method: "POST",
+    body: JSON.stringify({
+      recipients: recipients,
+      subject: subject,
+      body: body,
+    }),
+  })
+  .then((response) => {
+    response.json();
+  })
+  .then(() => {
+    load_mailbox('sent');
+  });
+
+  return false;
 }

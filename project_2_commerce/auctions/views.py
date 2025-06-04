884ec92afd2316c91864be756fd1ca1012bf3551
e0406370 <e0406370@u.nus.edu>
2025-06-04 23:36:42 +0800

    [refac] include keyword params and type hints

--- project_2_commerce/auctions/views.py
+++ project_2_commerce/auctions/views.py
@@ -3,69 +3,68 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
-from django.http import HttpResponseRedirect
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
 from auctions import util
-
 from .models import Bid, Comment, Listing, User
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
 
     listings = util.get_active_listings()
 
     return render(
         request, 
-        "auctions/index.html", 
-        {"listings": listings}
+        template_name="auctions/index.html", 
+        context={"listings": listings}
     )
 
 
-def categories(request):
+def categories(request: HttpRequest) -> HttpResponse:
 
     categories = util.get_categories()
 
     return render(
         request, 
-        "auctions/categories.html", 
-        {"categories": categories}
+        template_name="auctions/categories.html", 
+        context={"categories": categories}
     )
 
 
-def category_listing(request, category):
+def category_listing(request: HttpRequest, category: str) -> HttpResponse:
 
     listings = util.get_active_listings_by_category(category)
 
     return render(
         request,
-        "auctions/categorylisting.html",
-        {"listings": listings, "category": category},
+        template_name="auctions/categorylisting.html",
+        context={"listings": listings, "category": category},
     )
- 
+
     
-def view_listing(request, listing_id):
+def view_listing(request: HttpRequest, listing_id: int) -> HttpResponse:
     
     listing = util.get_listing_by_id(listing_id)
     user = request.user
-    
+
     is_in_watchlist = False
     is_listing_created_by_user = False
     is_highest_bidder = False
-    
+
     if user.is_authenticated:
         is_in_watchlist = util.is_listing_in_watchlist(user, listing)
         is_listing_created_by_user = util.is_listing_created_by_user(user, listing_id)
         is_highest_bidder = util.is_auction_winner(user, listing_id)
-                
+
     comments = util.get_comments_by_listing(listing)
     highest_bid = util.get_highest_bid(listing)
-    
+
     return render(
         request,
-        "auctions/listing.html",
-        {"listing": listing, 
+        template_name="auctions/listing.html",
+        context={"listing": listing, 
          "listing_id": listing_id, 
          "is_in_watchlist": is_in_watchlist,
          "comments": comments,
@@ -76,7 +75,8 @@ def view_listing(request, listing_id):
     )
 
 
-def login_view(request):
+def login_view(request: HttpRequest) -> HttpResponse | HttpResponseRedirect:
+
     if request.method == "POST":
 
         # Attempt to sign user in
@@ -84,26 +84,32 @@ def login_view(request):
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
 
-        # Check if authentication successful
+        # Check if authentication is successful
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse("index"))
+            return HttpResponseRedirect(reverse(viewname="index"))
         else:
             return render(
                 request,
-                "auctions/login.html",
-                {"message": "Invalid username and/or password."},
+                template_name="auctions/login.html",
+                context={"message": "Invalid username and/or password."},
             )
     else:
-        return render(request, "auctions/login.html")
+        return render(
+            request,
+            template_name="auctions/login.html"
+        )
 
 
-def logout_view(request):
+def logout_view(request: HttpRequest) -> HttpResponseRedirect:
+
     logout(request)
-    return HttpResponseRedirect(reverse("index"))
+
+    return HttpResponseRedirect(reverse(viewname="index"))
 
 
-def register(request):
+def register(request: HttpRequest) -> HttpResponse | HttpResponseRedirect:
+
     if request.method == "POST":
 
         username = request.POST["username"]
@@ -115,8 +121,8 @@ def register(request):
         if password != confirmation:
             return render(
                 request, 
-                "auctions/register.html", 
-                {"message": "Passwords must match."}
+                template_name="auctions/register.html", 
+                context={"message": "Passwords must match."}
             )
 
         # Attempt to create new user
@@ -126,14 +132,17 @@ def register(request):
         except IntegrityError:
             return render(
                 request,
-                "auctions/register.html",
-                {"message": "Username already taken."},
+                template_name="auctions/register.html",
+                context={"message": "Username already taken."},
             )
         login(request, user)
-        return HttpResponseRedirect(reverse("index"))
+        return HttpResponseRedirect(reverse(viewname="index"))
     
     else:
-        return render(request, "auctions/register.html")
+        return render(
+            request,
+            template_name="auctions/register.html"
+        )
 
 
 class ListingForm(forms.Form):
@@ -185,7 +194,7 @@ class ListingForm(forms.Form):
         required=False,
     )
 
-    def clean(self):
+    def clean(self) -> None:
         data = self.cleaned_data
 
         if data.get("title") is None:
@@ -199,7 +208,7 @@ class ListingForm(forms.Form):
 
 
 @login_required
-def create_listing(request):
+def create_listing(request: HttpRequest) -> HttpResponse | HttpResponseRedirect | HttpResponsePermanentRedirect:
 
     if request.method == "POST":
 
@@ -213,7 +222,7 @@ def create_listing(request):
             listing_categories = form.cleaned_data["categories"]
 
             if not listing_image_url:
-                listing_image_url = util.placeholder_image
+                listing_image_url = util.PLACEHOLDER_IMAGE
 
             new_listing = Listing.objects.create(
                 title=listing_title,
@@ -224,59 +233,66 @@ def create_listing(request):
                 creation_user=request.user,
             )
 
-            return redirect('listing', listing_id=new_listing.id)
+            return redirect(
+                to='listing',
+                listing_id=new_listing.id
+            )
 
         else:
             return render(
                 request, 
-                "auctions/createlisting.html", 
-                {"form": form}
+                template_name="auctions/createlisting.html",
+                context={"form": form}
             )
 
     return render(
         request, 
-        "auctions/createlisting.html", 
-        {"form": ListingForm()}
+        template_name="auctions/createlisting.html",
+        context={"form": ListingForm()}
     )
 
+
 @login_required
-def add_to_watchlist(request, listing_id):
+def add_to_watchlist(request: HttpRequest, listing_id: int) -> HttpResponseRedirect:
     
     selected_listing = util.get_listing_by_id(listing_id)
     
     util.add_listing_to_watchlist(request.user, selected_listing)
     
-    return HttpResponseRedirect(reverse("watchlist"))
+    return HttpResponseRedirect(reverse(viewname="watchlist"))
+
 
 @login_required
-def delete_from_watchlist(request, listing_id):
+def delete_from_watchlist(request: HttpRequest, listing_id: int) -> HttpResponseRedirect:
     
     selected_listing = util.get_listing_by_id(listing_id)
     
     util.delete_listing_from_watchlist(request.user, selected_listing)
     
-    return HttpResponseRedirect(reverse("watchlist"))
+    return HttpResponseRedirect(reverse(viewname="watchlist"))
+
 
 @login_required
-def view_watchlist(request):
+def view_watchlist(request: HttpRequest):
     
     watchlist_listings = util.get_watchlist_by_user(request.user)
     
     return render(
         request, 
-        "auctions/watchlist.html",
-        {"watchlist": watchlist_listings}
+        template_name="auctions/watchlist.html",
+        context={"watchlist": watchlist_listings}
     )
-    
+
+
 @login_required
-def add_comment(request, listing_id):
-    
+def add_comment(request: HttpRequest, listing_id: int) -> HttpResponseRedirect | HttpResponsePermanentRedirect:
+
     selected_listing = util.get_listing_by_id(listing_id)
     current_user = request.user
-    
-    if (request.method == "POST"):
-        
-        comment_text = request.POST.get("comment")
+
+    if request.method == "POST":
+
+        comment_text = request.POST["comment"]
         
         new_comment = Comment(
             comment_text=comment_text,
@@ -284,46 +300,58 @@ def add_comment(request, listing_id):
             comment_listing=selected_listing,
         )
         new_comment.save()
-        
-    return redirect('listing', listing_id=selected_listing.id)
+
+    return redirect(
+        to='listing',
+        listing_id=selected_listing.id
+    )
 
 
 @login_required
-def add_bid(request, listing_id):
-    
+def add_bid(request: HttpRequest, listing_id: int) -> HttpResponseRedirect | HttpResponsePermanentRedirect:
+
     selected_listing = util.get_listing_by_id(listing_id)
     current_user = request.user
-    
-    if (request.method == "POST"):
-        
-        bid_amount = request.POST.get("bid")
-        
+
+    if request.method == "POST":
+
+        bid_amount = request.POST["bid"]
+
         if util.is_valid_bid(float(bid_amount), selected_listing):
-            
+
             new_bid = Bid(
                 bid_amount=bid_amount,
                 bid_user=current_user,
                 bid_listing=selected_listing,
             )
             new_bid.save()
-            
-            return redirect('listing', listing_id=selected_listing.id)
-        
+
+            return redirect(
+                to='listing',
+                listing_id=selected_listing.id
+            )
+
         else:
             error_message = "Invalid bid amount. Bid must be higher than the starting bid and the highest bid."
             messages.error(request, error_message)
-            
-            return redirect('listing', listing_id=selected_listing.id)
-        
-    return redirect('listing', listing_id=selected_listing.id)
+
+            return redirect(
+                to='listing', 
+                listing_id=selected_listing.id
+            )
+
+    return redirect(
+        to='listing',
+        listing_id=selected_listing.id
+    )
 
 
 @login_required
-def close_auction(request, listing_id):
-    
+def close_auction(request: HttpRequest, listing_id: int):
+
     selected_listing = util.get_listing_by_id(listing_id)
-    
+
     selected_listing.active_state = False
     selected_listing.save()
-    
-    return HttpResponseRedirect(reverse("index"))
+
+    return HttpResponseRedirect(reverse(viewname="index"))
